e732409bf4d6294481ee10491ebbad7adf8d36b2
10Narratives <vanyatertyshnikov@yandex.ru>
2025-05-05 23:26:55 +0300

    Start worker node

---
+++ internal/worker_node/cmd/worker_node/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	// TODO: Read configuration
+
+	// TODO: Initialize logger instance
+
+	// TODO: Initialize gRPC-server app
+
+	// TODO: Initialize worker node app
+}
