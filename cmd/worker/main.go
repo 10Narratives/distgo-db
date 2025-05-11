f917df053fc928a79885257547b62240f7e40910
10Narratives <vanyatertyshnikov@yandex.ru>
2025-05-11 14:02:05 +0300

    Add logging to worker node

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"githib.com/10Narratives/distgo-db/internal/worker/config"
+	"githib.com/10Narratives/distgo-db/internal/worker/logging"
 )
 
 func main() {
@@ -12,10 +13,14 @@ func main() {
 	fmt.Println(cfg)
 
 	// TODO: initialize logger
+	log := logging.NewLogger(cfg.Logging.Format, cfg.Logging.Level)
+	log.Info("starting worker node #1")
 
 	// TODO: initialize storage app
 
 	// TODO: initialize gRPC-server app
 
 	// TODO: initialize worker app
+
+	log.Info("worker node #1 stopped")
 }
