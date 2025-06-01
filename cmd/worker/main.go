d999bdfcb192b77d21b44b1c82b6b341c10e2bce
10Narratives <vanyatertyshnikov@yandex.ru>
2025-06-01 11:44:32 +0300

    Add logging for worker node

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -4,13 +4,20 @@ import (
 	"fmt"
 
 	workercfg "github.com/10Narratives/distgo-db/internal/config/worker"
+	"github.com/10Narratives/distgo-db/internal/lib/logging/sl"
 )
 
 func main() {
 	cfg := workercfg.MustLoad()
 	fmt.Println(cfg)
 
-	// TODO: initialize logger
+	log := sl.MustLogger(
+		sl.WithLevel(cfg.Logging.Level),
+		sl.WithFormat(cfg.Logging.Format),
+		sl.WithOutput(cfg.Logging.Output),
+	)
+	log.Info("Worker " + cfg.Name + " is online")
+
 	// TODO: initialize gRPC application
 	// TODO: initialize application
 }
