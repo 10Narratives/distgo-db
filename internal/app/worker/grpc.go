863f2577bfd87731602edbab6c49fc47bae8a912
10Narratives <vanyatertyshnikov@yandex.ru>
2025-06-04 20:44:37 +0300

    Add struct of worker app and grpc app

---
+++ internal/app/worker/grpc.go
@@ -0,0 +1,84 @@
+package workerapp
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"net"
+
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/recovery"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type GRPCApp struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func NewGRPCApp(log *slog.Logger, port int) *GRPCApp {
+	loggingOpts := []logging.Option{
+		logging.WithLogOnEvents(
+			logging.PayloadReceived, logging.PayloadSent,
+		),
+	}
+
+	recoveryOpts := []recovery.Option{
+		recovery.WithRecoveryHandler(func(p interface{}) (err error) {
+			log.Error("Recovered from panic", slog.Any("panic", p))
+			return status.Errorf(codes.Internal, "internal error")
+		}),
+	}
+
+	gRPCServer := grpc.NewServer(grpc.ChainUnaryInterceptor(
+		recovery.UnaryServerInterceptor(recoveryOpts...),
+		logging.UnaryServerInterceptor(InterceptorLogger(log), loggingOpts...),
+	))
+
+	return &GRPCApp{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
+
+func InterceptorLogger(l *slog.Logger) logging.Logger {
+	return logging.LoggerFunc(func(ctx context.Context, lvl logging.Level, msg string, fields ...any) {
+		l.Log(ctx, slog.Level(lvl), msg, fields...)
+	})
+}
+
+func (g *GRPCApp) MustRun() {
+	if err := g.Run(); err != nil {
+		panic(err)
+	}
+}
+
+func (g *GRPCApp) Run() error {
+	const op = "worker.grpcapp.Run"
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", g.port))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	g.log.Info("grpc server started", slog.String("addr", l.Addr().String()))
+
+	if err := g.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (a *GRPCApp) Stop() {
+	const op = "worker.grpcapp.Stop"
+
+	a.log.With(slog.String("op", op)).
+		Info("stopping gRPC server", slog.Int("port", a.port))
+
+	a.gRPCServer.GracefulStop()
+}
