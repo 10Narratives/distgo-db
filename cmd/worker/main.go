e55144cc5114bae2c64f772ed4726e80453f5d1f
10Narratives <vanyatertyshnikov@yandex.ru>
2025-06-05 11:18:31 +0300

    Make cfg reading and logging for worker

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,37 +1,17 @@
 package main
 
-// import (
-// 	"os"
-// 	"os/signal"
-// 	"syscall"
+import (
+	workercfg "github.com/10Narratives/distgo-db/internal/config/worker"
+	"github.com/10Narratives/distgo-db/internal/lib/logger/sl"
+)
 
-// 	workerapp "github.com/10Narratives/distgo-db/internal/app/worker"
+func main() {
+	cfg := workercfg.MustLoad()
 
-// 	workercfg "github.com/10Narratives/distgo-db/internal/config/worker"
-// 	"github.com/10Narratives/distgo-db/internal/lib/logging/sl"
-// )
-
-// func main() {
-// 	cfg := workercfg.MustLoad()
-
-// 	log := sl.MustLogger(
-// 		sl.WithLevel(cfg.Logging.Level),
-// 		sl.WithFormat(cfg.Logging.Format),
-// 		sl.WithOutput(cfg.Logging.Output),
-// 	)
-// 	log.Info("Worker " + cfg.Name + " is online")
-
-// 	application := workerapp.New(log, cfg)
-
-// 	go func() {
-// 		application.GRPCServer.MustRun()
-// 	}()
-
-// 	stop := make(chan os.Signal, 2)
-// 	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
-
-// 	<-stop
-
-// 	application.GRPCServer.Stop()
-// 	log.Info("Worker " + cfg.Name + " is stopped")
-// }
+	log := sl.MustLogger(
+		sl.WithFormat(cfg.Logging.Format),
+		sl.WithOutput(cfg.Logging.Output),
+		sl.WithLevel(cfg.Logging.Level),
+	)
+	log.Info(cfg.Name + " is online")
+}
---
+++ internal/config/worker/config.go
@@ -0,0 +1,38 @@
+package workercfg
+
+import (
+	"flag"
+	"os"
+
+	"github.com/10Narratives/distgo-db/internal/config"
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Name    string              `yaml:"name" env-required:"true"`
+	GRPC    config.GRPCConfig   `yaml:"grpc"`
+	Logging config.LoggerConfig `yaml:"logging"`
+}
+
+func MustLoad() *Config {
+	var path string
+
+	flag.StringVar(&path, "config", "", "path to configuration file")
+	flag.Parse()
+
+	return MustLoadFromFile(path)
+}
+
+func MustLoadFromFile(path string) *Config {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("config file does not exist: " + path)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(path, &cfg); err != nil {
+		panic("cannot read config: " + err.Error())
+	}
+
+	return &cfg
+}
