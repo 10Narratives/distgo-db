02eb9d1a9d725050ea69ceb4c97e19fc466f0f40
10Narratives <vanyatertyshnikov@yandex.ru>
2025-06-15 09:18:21 +0300

    Add key models

---
+++ internal/models/worker/data/database/key.go
@@ -0,0 +1,14 @@
+package databasemodels
+
+import "github.com/10Narratives/distgo-db/internal/lib/grpc/utils"
+
+type Key struct {
+	Database string
+}
+
+func NewKey(name string) Key {
+	parsed := utils.ParseName(name)
+	return Key{
+		Database: parsed.DatabaseID,
+	}
+}
