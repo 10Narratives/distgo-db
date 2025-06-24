e03723cd9d544d725f8e00ab621b1a06294b842a
10Narratives <vanyatertyshnikov@yandex.ru>
2025-06-24 23:52:45 +0300

    Delete all old version of project

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-## [v0.1.0] - 2025-04-05
-### Added
-- Implementation of CRUD operations for documents, collections, and databases.
-- In-memory storage with goroutine-safe access.
-- gRPC services for data operations.
--- internal/models/worker/data/transaction/transaction.go
+++
@@ -1,8 +0,0 @@
-package transactionmodels
-
-import commonmodels "github.com/10Narratives/distgo-db/internal/models/worker/data/common"
-
-type Transaction struct {
-	ID         string
-	Operations []commonmodels.Operation
-}
